3eedbf79b61aa9c36bef601b19199386f4921800
Bobby <bobbypatel94@gmail.com>
2024-02-24 20:14:59 +0530

    destination package test completed

--- src/test/DestinationTest.java
+++ src/test/DestinationTest.java
@@ -8,20 +8,27 @@ import packages.Activity;
 import packages.Destination;
 
 public class DestinationTest {
+    Destination destination = new Destination("Kullu");
+    Activity activity = new Activity("Skating" , "enjoy skaing in slopes of mountain" , 1000 , 5 , destination);
 
-    @Test
-    void testAddDestination(){
-        String name = "Rohtang";
-        Destination destination = new Destination(name);
-        assertEquals(destination.getName() , name);
-    }
 
     @Test
     void testAddActivity(){
-        Destination destination = new Destination("Kullu");
-        Activity activity = new Activity("Skating" , "enjoy skaing in slopes of mountain" , 1000 , 5 , destination);
+        
         destination.addActivity(activity);
 
         assertTrue(destination.getActivities().contains(activity));
+    }
+
+
+    @Test
+    void testGetActivity(){
+        destination.addActivity(activity);
+        assertEquals(activity , destination.getActivities().get(0));
+    }
+
+    @Test
+    void testGetName(){
+        assertEquals(destination.getName(), "Kullu");
     }
 }
